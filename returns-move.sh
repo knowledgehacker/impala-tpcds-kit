9a13e08ff97bed6fd7990a086d0c7670a91414e5
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2014-11-14 17:05:00 +0100

    Make script executable.

--- returns-move.sh
+++ returns-move.sh
@@ -8,4 +8,3 @@ mkdir -p ${FLATFILE_ROOT}/web_returns
 mv ${FLATFILE_ROOT}/catalog_sales/catalog_returns* ${FLATFILE_ROOT}/catalog_returns
 mv ${FLATFILE_ROOT}/store_sales/store_returns* ${FLATFILE_ROOT}/store_returns
 mv ${FLATFILE_ROOT}/web_sales/web_returns* ${FLATFILE_ROOT}/web_returns
-
